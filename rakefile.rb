988b6b4b4c99f293c5b027b72a7105d102871ad4
Thomas Fannes <thomas.fannes@cs.kuleuven.be>
2015-05-05 16:27:14 +0200

    travis build file

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,11 @@
 $build_type=:debug
+$library_dir="/home/thomasf/boost/lib"
 
 namespace :test do
     def fff_all_tests(*options)
         libs = %w[boost_system boost_program_options].map{|lib|"c++.library:#{lib}"}*' '
         FileList.new('**/test/*.cpp').each do |fn|
-            sh "fff c++.tree:mlpp #{libs} #{fn} #{options*' '}"
+            sh "fff c++.tree:mlpp #{libs} #{fn} #{options*' '} c++.library_path:#{$library_dir}"
         end
     end
     task :build do
