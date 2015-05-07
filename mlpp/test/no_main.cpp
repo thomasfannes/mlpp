fbb6200a78b2595de4999cb25ef875d94a48ecd2
Thomas Fannes <thomas.fannes@cs.kuleuven.be>
2015-05-07 12:05:36 +0200

    Added noncompilable tests and support in rakefile

---
+++ mlpp/test/no_main.cpp
@@ -0,0 +1,9 @@
+#include <mlpp/mlpp.hpp>
+#include <iostream>
+#include <boost/program_options.hpp>
+
+int main(int argc, char ** argv)
+{
+	int a = static_cast<std::string>("aaaa");
+ 	return 0;
+}
--- rakefile.rb
+++ rakefile.rb
@@ -1,20 +1,66 @@
 $build_type=:debug
-$library_dir="/home/thomasf/boost/lib"
+$library_dirs=%w[/home/thomasf/boost/lib]
+$libs = %w[boost_system boost_program_options]
+$include_paths = %w[mlpp]
+
 
 namespace :test do
-    def fff_all_tests(*options)
-        libs = %w[boost_system boost_program_options].map{|lib|"c++.library:#{lib}"}*' '
+	def executeCommand(cmd)
+		output = ""
+		IO.popen(cmd + " 2>&1") do |io|
+			while line = io.gets
+				output += line.chomp!
+			end
+			
+			io.close
+			return output, $?.to_i
+		end
+	end
+	
+    def fff_all_tests(execute_tests)
+		$stdout.sync = true
+		
+        libs = $libs.map{ |lib| "c++.library:#{lib}"}*' '
+        include_paths = $include_paths.map{ |path| "c++.tree:#{path}"}*' '
+        library_dirs = $library_dirs.map{ |dir| "c++.library_path:#{dir}"}*' '
+        
+        execTestMSG = execute_tests ? "Building and executing " : "Building "
+        
         FileList.new('**/test/*.cpp').each do |fn|
-            sh "fff c++.tree:mlpp #{libs} #{fn} #{options*' '} c++.library_path:#{$library_dir}"
+			executableTest = fn.pathmap("%f").start_with?("no_") ? false : true
+			
+			if executableTest
+				print execTestMSG + "test '#{fn}': "
+			else
+				print "Building noncompilable test '#{fn}': "
+			end
+						
+			command = "fff #{libs} #{include_paths} #{library_dirs} #{fn}" + ( (execute_tests and executableTest) ? "" : " norun")
+			output,code = executeCommand(command)
+					
+			if (code == 0) == executableTest
+				puts "OK"
+			else
+				if executableTest
+					puts "Failed"
+					puts output
+				else
+					puts "Able to compile"
+				end	
+				fail "Test failed"
+			end
         end
     end
+    
     task :build do
-        fff_all_tests('norun', $build_type)
+        fff_all_tests(false)
     end
+    
     task :run do
-        fff_all_tests($build_type)
+        fff_all_tests(true)
     end
 end
+
 task :test => 'test:run'
 
 namespace :qtcreator do
